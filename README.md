ed587a8eefd99059b68495db2ec6cbfb2fd8ca7c
Kamil <mr.kaam@gmail.com>
2023-09-04 10:43:11 +0200

    Section2: create new views

--- README.md
+++ README.md
@@ -22,3 +22,7 @@ Course link: https://www.udemy.com/course/symfony-tworzenie-aplikacji-php-od-pod
 3.  NPM
 4.  Symfony `composer create project symfony/skeleton: "6.2.*`
 5.  Local-php-security-checker `composer require symfony/requirements-checker --dev`
+
+### Routing and Views
+
+-   In this section I made first controller, routes and views
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,51 +3,55 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    // #[Route('/post', name: 'app_post')]
-    // public function index(): JsonResponse
-    // {
-    //     return $this->json([
-    //         'message' => 'Welcome to your new controller!',
-    //         'path' => 'src/Controller/PostController.php',
-    //     ]);
-    // }
-
     #[Route('/', methods:['GET'], name: 'posts.index')] // to odpowiada stronir głównej
     public function inex() : Response
     {
-        return new Response(
-            "<h1>Lista wszystkich postów!</h1>" .
-            "<p>Nazwa route'a użyta w tym widoku:    " .
-            $this->generateUrl('posts.index') .  "</p>"
-        );
+        return $this->render('post/index.html.twig');
+    }
+
+    // Wczytywanie new.html.twig
+    #[Route('/post/new' , methods: ['GET' , 'POST'] , name: 'posts.new')]
+    public function new() : Response
+    {
+        return $this->render('post/new.html.twig');
+    }
+
+    // Wyświetlanie szczegółów posta na blogu
+    #[Route('/post/{id}' , methods: ['GET'] , name: 'posts.show')]
+    public function show($id) : Response
+    {
+        return $this->render('post/show.html.twig');
     }
 
     // Za pomocą tego rout'a wyciągniemy wszystkie posty ale konkretnego użytkownika (konkretne ID)
     #[Route('/posts/user/{id}' , methods: ['GET'] , name: 'posts.user')]
     public function user(int $id) : Response
     {
-        return new Response(
-            '<h1>Lista wszystkich postów konkretnego użytkownika</h1>' .
-            'User id: ' . $id . '<br' .
-            'Named route that we will use in the view: ' .
-            $this->generateUrl('posts.user' , ['id' => $id])
-        );
+        return $this->render('post/index.html.twig');
+    }
+
+    #[Route('post/{id}/edit' , methods: ['GET'] , name: 'posts.edit')]
+    public function edit($id) : Response
+    {
+        // return $this->redirectToRoute('posts.index');
+        return $this->render('post/edit.html.twig');
+    }
+
+    #[Route('/post/{id}/delete' , methods: ['POST'] , name: 'posts.delete')]
+    public function delete($id) : Response
+    {
+        return new Response('delete post from the database');
     }
 
     // Za pomoca tego route'a będziemy przełączali czy użytkownika śledzimy czy nie
     #[Route('/toggleFollow/{user}' , methods: ['GET'] , name: 'toggleFollow')]
     public function toggleFollow($user) : Response
     {
-        return new Response(
-            '<h1>Toggle like/dislike</h1>' .
-            '<p>User id: ' . $user . '</br>' .
-            $this->generateUrl('toggleFollow' , ['user' => $user]) . '</p>');
-
+        return new Response('logic for toggling like/dislike functionality');
     }
 }
